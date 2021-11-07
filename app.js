e6997be20a02b4cfaea8d3966607114b4f1a16d2
santhoshjay <hackerbparker@gmail.com>
2021-11-07 16:58:56 +0530

    l1 - plain password authentication

---
+++ app.js
@@ -0,0 +1,75 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const { userInfo } = require("os");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/secretsDB");
+
+const userSchema = {
+    email: String,
+    password: String
+}
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", function(req,res){
+    res.render("home");
+});
+
+app.get("/secrets", function(req,res){
+    res.render("secrets");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req,res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save(function(err){
+        if(!err){
+            console.log("New User Document inserted successfully");
+            res.render("register");
+        }
+        else
+            console.log(err);
+    });
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.post("/login", function(req,res){
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, user){
+        if(!err && user){
+            if(user.password === password)
+                res.render("secrets");
+            }
+            else{
+                console.log(err);
+                res.render("login");
+            }
+    });
+});
+
+app.listen(3000, function(){
+    console.log("Server has started and is listening on port 3000");
+});
