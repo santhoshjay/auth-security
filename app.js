f785972aab26f6b0d4ad66394454c78a6653b96f
santhoshjay <hackerbparker@gmail.com>
2021-11-07 18:28:36 +0530

    L3 - md5-hashing authentication

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const { userInfo } = require("os");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -19,8 +19,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = mongoose.model("User", userSchema);
 
@@ -39,7 +37,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req,res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err){
@@ -59,7 +57,7 @@ app.get("/login", function(req, res){
 app.post("/login", function(req,res){
 
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err, user){
         if(!err && user){
